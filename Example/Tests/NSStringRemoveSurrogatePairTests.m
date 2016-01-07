ff887dedcf35071b229744eec95e7cef0b2dfc6c
Mark Rogers <mark.rogers@yodle.com>
2016-01-07 12:49:29 -0500

    Tidied Readme, pod spec, and tests.

--- Example/Tests/NSStringRemoveSurrogatePairTests.m
+++ Example/Tests/NSStringRemoveSurrogatePairTests.m
@@ -15,50 +15,58 @@
 
 @implementation NSStringRemoveSurrogatePairTests
 
-- (void)testContainsSurrogate {
-	XCTAssertFalse([@"Business Professional ☺️" containsSurrogatePair]);
+- (void)testContainsSurrogate
+{
+	XCTAssertFalse([@"Text ☺️" containsSurrogatePair]);
 	XCTAssert([@"🌍" containsSurrogatePair]);
-	XCTAssert([@"🌍Business Professional ☺️" containsSurrogatePair]);
+	XCTAssert([@"🌍Text ☺️" containsSurrogatePair]);
 }
 
-- (void)testRemovedSurrogateString {
-	XCTAssert([[@"Business Professional ☺️" stringByRemovingSurrogatePairs] isEqualToString:@"Business Professional ☺️"]);
+- (void)testRemovedSurrogateString
+{
+	XCTAssert([[@"Text ☺️" stringByRemovingSurrogatePairs] isEqualToString:@"Text ☺️"]);
 	XCTAssert([[@"🌍" stringByRemovingSurrogatePairs] isEqualToString:@""]);
-	XCTAssert([[@"🌍Business Professional ☺️" stringByRemovingSurrogatePairs] isEqualToString:@"Business Professional ☺️"]);
+	XCTAssert([[@"🌍Text ☺️" stringByRemovingSurrogatePairs] isEqualToString:@"Text ☺️"]);
 }
 
-- (void)testFirstNonSurrogatePairEmojiIsNotSurrogatePair {
+- (void)testFirstNonSurrogatePairEmojiIsNotSurrogatePair
+{
 	NSString* unicodeString = [NSString stringWithUTF8String:"\U00002100"]; // ℀
 	XCTAssertFalse([unicodeString containsSurrogatePair]);
 	XCTAssertFalse([[unicodeString stringByRemovingSurrogatePairs] isEqualToString:@""]);
 }
 
-- (void)testLastNonSurrogatePairEmojiIsNotSurrogatePair {
+- (void)testLastNonSurrogatePairEmojiIsNotSurrogatePair
+{
 	NSString* unicodeString = [NSString stringWithUTF8String:"\U000027BF"]; // ➿
 	XCTAssertFalse([unicodeString containsSurrogatePair]);
 	XCTAssertFalse([[unicodeString stringByRemovingSurrogatePairs] isEqualToString:@""]);
 }
 
-- (void)testLastNonSurrogateCharacterIsNotSurrogatePair {
+- (void)testLastNonSurrogateCharacterIsNotSurrogatePair
+{
 	NSString* unicodeString = [NSString stringWithUTF8String:"\U0000D7FF"]; // ퟿
 	XCTAssertFalse([unicodeString containsSurrogatePair]);
 	XCTAssertFalse([[unicodeString stringByRemovingSurrogatePairs] isEqualToString:@""]);
 }
 
-- (void)testFirstSurrogatePairUnicodeCharacterIsSurrogatePair {
+- (void)testFirstSurrogatePairUnicodeCharacterIsSurrogatePair
+{
 	NSString* unicodeString = [NSString stringWithUTF8String:"\U0000E000"]; // 
 	XCTAssert([unicodeString containsSurrogatePair]);
 	XCTAssert([[unicodeString stringByRemovingSurrogatePairs] isEqualToString:@""]);
 }
 
-- (void)testFirstThreeByteUnicodeCharacterIsSurrogatePair {
+- (void)testFirstThreeByteUnicodeCharacterIsSurrogatePair
+{
 	NSString* unicodeString = [NSString stringWithUTF8String:"\U00010000"]; // 𐀀
 	NSLog(@"\n\nUnicode: %@\n\n",unicodeString);
 	XCTAssert([unicodeString containsSurrogatePair]);
 	XCTAssert([[unicodeString stringByRemovingSurrogatePairs] isEqualToString:@""]);
 }
 
-- (void)testLowestUnicodeValueEmojiIsSurrogatePair {
+- (void)testLowestUnicodeValueEmojiIsSurrogatePair
+{
 	NSString* unicodeString = [NSString stringWithUTF8String:"\U0001F004"]; // 🀄️
 	XCTAssert([unicodeString containsSurrogatePair]);
 	XCTAssert([[unicodeString stringByRemovingSurrogatePairs] isEqualToString:@""]);
--- NSString-SurrogatePair.podspec
+++ NSString-SurrogatePair.podspec
@@ -1,42 +1,17 @@
-#
-# Be sure to run `pod lib lint NSString-SurrogatePair.podspec' to ensure this is a
-# valid spec before submitting.
-#
-# Any lines starting with a # are optional, but their use is encouraged
-# To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
-#
-
 Pod::Spec.new do |s|
   s.name             = "NSString-SurrogatePair"
-  s.version          = "0.1.0"
-  s.summary          = "Two categories for NSString that allow screening for surrogate pairs."
-
-# This description is used to generate tags and improve search results.
-#   * Think: What does it do? Why did you write it? What is the focus?
-#   * Try to keep it short, snappy and to the point.
-#   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!  
+  s.version          = "1.0.0"
+  s.summary          = "An NSString category to identify and remove surrogate pair UTF-8 characters."
   s.description      = <<-DESC
-                       Two categories for NSString that allow screening for emojis and surrogate pairs.
+                       This is to support the unicode character set in MySQL which handles a 
+                       maximum of three bytes per multibyte character.
                        This is loosely based on another pod: woxtu/NSString-RemoveEmoji
-
-                       * Markdown format.
-                       * Don't worry about the indent, we strip it!
                        DESC
-
   s.homepage         = "https://github.com/yodle/NSString-SurrogatePair"
-  # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
-  s.license          = 'MIT'
-  s.author           = { "Mark Rogers" => "mark.rogers@yodle.com" }
+  s.license          = { type: 'MIT', file: 'LICENSE.md' }
+  s.author           = { "Yodle" => "mobsters@yodle.com" }
   s.source           = { :git => "https://github.com/yodle/NSString-SurrogatePair.git", :tag => s.version.to_s }
-
-  s.platform     = :ios, '7.0'
+  s.platform     = :ios, '8.0'
   s.requires_arc = true
-
   s.source_files = 'Pod/Classes/**/*'
-  s.resource_bundles = {
-    'NSString-RemoveEmoji' => ['Pod/Assets/*.png']
-  }
-
-  # s.public_header_files = 'Pod/Classes/**/*.h'
 end
