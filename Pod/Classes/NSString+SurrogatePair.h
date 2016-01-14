e2391a2acfe549175e19d7ea6fa4546d4de0b7ef
Mark Rogers <mark.rogers@yodle.com>
2016-01-14 16:34:58 -0500

    Added documentation to header file.

--- Pod/Classes/NSString+SurrogatePair.h
+++ Pod/Classes/NSString+SurrogatePair.h
@@ -8,7 +8,18 @@
 
 @interface NSString (SurrogatePair)
 
+/**
+ *  Filters all surrogate pair characters out of the string.
+ *
+ *  @return NSString	An exact replica of the original string, without any surrogate pair characters
+ */
 - (instancetype)stringByRemovingSurrogatePairs;
+
+/**
+ *  Determines whether or not a string contains any surrogate pair characters.
+ *
+ *  @return BOOL	Indicates if the string does contain at least one surrogate pair character.
+ */
 - (BOOL)containsSurrogatePair;
 
 @end
